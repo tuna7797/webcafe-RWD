0f4b9d65b5fbf0ee7ce1d3ce9cfe2ba2b756f918
tuna7797 <gmlwjd7731@gmail.com>
2023-06-09 15:26:36 +0900

    카드 애니메이션 예제

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,6 @@
 const button = document.querySelector(".control-ani");
 const visual = document.querySelector(".visual");
+
 button.addEventListener("click", () => {
   visual.classList.toggle("is--active");
 });
