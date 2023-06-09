f0afe830f0a3aa795efba5faddf8afcd045d0195
tuna7797 <gmlwjd7731@gmail.com>
2023-06-09 15:11:09 +0900

    애니메이션 제어를 위한 자바스크립트 추가

---
+++ js/animation.js
@@ -0,0 +1,5 @@
+const button = document.querySelector(".control-ani");
+const visual = document.querySelector(".visual");
+button.addEventListener("click", () => {
+  visual.classList.toggle("is--active");
+});
