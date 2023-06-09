80a09f6531ea2e7ff778f99bb4d70f16b4743a52
tuna7797 <gmlwjd7731@gmail.com>
2023-06-10 00:07:29 +0900

    플립 카드 애니메이션 완성

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,12 @@
 const button = document.querySelector(".control-ani");
 const visual = document.querySelector(".visual");
+const flipButton = document.querySelector(".flip-button");
+const card = document.querySelector(".card");
 
 button.addEventListener("click", () => {
   visual.classList.toggle("is--active");
+});
+
+flipButton.addEventListener("click", () => {
+  card.classList.toggle("is--flipped");
 });
